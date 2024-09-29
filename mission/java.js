14a736527520a8a9dd3d367ff20794ab402f2ff3
wesleyleehopkins <wesley.hopkins.lds@gmail.com>
2024-09-28 21:39:04 -0600

    Update mission page with dark mode functionality and update logo image

--- mission/java.js
+++ mission/java.js
@@ -1,6 +1,7 @@
 // Select the body element that uses the container classes
 const bodyElement = document.querySelector('body');
 
+const logoElement = document.getElementById('logo');
 // Create a function to toggle classes based on user's input
 function toggleContainerClass() {
   const userChoice = prompt("Enter 'white' for light mode or 'black' for dark mode:");
@@ -8,9 +9,11 @@ function toggleContainerClass() {
   if (userChoice.toLowerCase() === 'black') {
     bodyElement.classList.remove('containerw');
     bodyElement.classList.add('containerb');
+    logoElement.src = 'images/byui-logo_black.jpg';
   } else if (userChoice.toLowerCase() === 'white') {
     bodyElement.classList.remove('containerb');
     bodyElement.classList.add('containerw');
+    logoElement.src = 'images/byui-logo_blue.webp';
   } else {
     alert('Invalid input! Please enter either "white" or "black".');
   }
